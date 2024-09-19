5aab49938321bebb5350f129cf12aa6b6618ed20
kaviya2321 <kaviyaprasanna02@gmail.com>
2024-09-19 22:19:50 +0530

    Biodata.c

---
+++ Biodata.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+     printf("Name          : M.Kaviya\n");
+     printf("Father's name : S.Murugesan/n");
+     printf("Mother's name : M.Selvi\n");
+     printf("Date of birth : 21/04/2007\n");
+     printf("Blood group   : O+ve\n");
+     printf("Address       : 38, Kottapulipalayam road\n");
+     printf("city          : Dharapuram\n");
+     printf("District      : Tiruppur\n");
+     printf("pincode       : 638656\n");
+     printf("contact       : 7904071939\n");
+     printf("Email id      : kaviyaprasanna02@gmail.com\n");
+     printf("Education     : B.E.Computer Science and Engineering\n"); 
+     printf("College       : K.S.Rangasamy College of technology\n");
+     printf("Hobby         : Reading, drawing\n");
+     printf("language      : Tamil, english");
+     return 0;
+}
