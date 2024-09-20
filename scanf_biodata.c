0cb513217acc4209a994a50405879984728b25ad
kaviya2321 <kaviyaprasanna02@gmail.com>
2024-09-20 21:24:16 +0530

    Update

---
+++ scanf_biodata.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int age;
+   printf("age:");
+   scanf("%d",&age);
+   float height,weight;
+   printf("height:");
+   scanf("%f",&height);
+   printf("weight:");
+   scanf("%f",&weight);
+   char gender;
+   printf("gender:");
+   scanf(" %c",&gender);
+   return 0;
+}
